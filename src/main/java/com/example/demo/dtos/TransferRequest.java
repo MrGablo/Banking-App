9f2b2350cc632ef333a44993c629765d2930d8c7
MrGablo <eahgabrieltor@gmail.com>
2026-05-03 13:05:29 +0200

    added back transfer request useremail for testing purposes even though its wrong to have it here.

--- src/main/java/com/example/demo/dtos/TransferRequest.java
+++ src/main/java/com/example/demo/dtos/TransferRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
 public record TransferRequest(
+        @NotBlank String userEmail,
         @NotBlank String fromIban,
         @NotBlank String toIban,
         @Positive double amount
